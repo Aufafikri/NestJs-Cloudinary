268cc59e0b80d68b7c02b74bb5c0daf87e56fba2
Aufa Fikri <fikriilmi6@gmail.com>
2025-06-15 16:16:07 +0700

    prisma setup

---
+++ src/libs/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    onModuleInit() {
+        return this.$connect()
+    }
+
+    onModuleDestroy() {
+        return this.$disconnect()
+    }
+}
